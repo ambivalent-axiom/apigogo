04eb965fb3804ef3d4b68f2fdd454c9f06cc2054
ambivalent-axiom <artmelnis@gmail.com>
2024-05-19 00:25:56 +0300

    done

--- ApiGoGo.php
+++ ApiGoGo.php
@@ -1,1 +1,91 @@
 <?php
+//dependencies
+require 'Exception.php';
+require 'PHPMailer.php';
+require 'SMTP.php';
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+class ApiGoGo
+{
+    protected string $apikey;
+    protected string $url;
+    public function __construct(string $url, string $apikey = "")
+    {
+        $this->apikey = $apikey;
+        $this->url = $url;
+    }
+    protected function doRequest(string $url): string
+    {
+        $request = curl_init();
+        curl_setopt($request, CURLOPT_URL, $url);
+        curl_setopt($request, CURLOPT_RETURNTRANSFER, true);
+        if( ! $result = curl_exec($request))
+        {
+            trigger_error(curl_error($request));
+        }
+        curl_close($request);
+        return $result;
+    }
+}
+class CatApi extends ApiGoGo
+{
+    public function getRandom(string $type, int $amount): string {
+        $endpoint = "$this->url/facts/random?animal_type=$type&amount=$amount";
+        return ApiGoGo::doRequest($endpoint);
+    }
+}
+class Agify extends ApiGoGo
+{
+    public string $name;
+    public function __construct(string $url, string $name, string $apikey = "")
+    {
+        parent::__construct($url, $apikey);
+        $this->name = $name;
+    }
+    public function getAgify(): string
+    {
+        $endpoint = "$this->url?name=$this->name";
+        return ApiGoGo::doRequest($endpoint);
+    }
+}
+class EmailValidation extends ApiGoGo
+{
+    public string $email;
+    public function __construct(string $url, string $email, string $apikey = "") {
+        parent::__construct($url, $apikey);
+        $this->email = $email;
+    }
+    public function validate(): string
+    {
+        $endpoint = "$this->url/v1/info?email=$this->email&apikey=$this->apikey";
+        return ApiGoGo::doRequest($endpoint);
+    }
+}
+
+function sendEmailNotification(string $from, string $subject, string $content, string $recipient): void {
+    $mail = new PHPMailer(true);
+    try {
+        // Server settings
+        //$mail->SMTPDebug = 2;
+        $mail->isSMTP();
+        $mail->Host       = 'mail.inbox.lv';
+        $mail->SMTPAuth   = true;
+        $mail->Username   = 'ambax.io@inbox.lv';
+        $mail->Password   = 'JapsTQ786R'; //here pass
+        $mail->SMTPSecure = 'ssl';
+        $mail->Port       = 465;
+
+        // Recipients
+        $mail->setFrom('ambax.io@inbox.lv', $from);
+        $mail->addAddress($recipient); //here the address to.
+
+        // Content
+        $mail->isHTML(true);
+        $mail->Subject = $subject;
+        $mail->Body    = $content;
+        $mail->send();
+    } catch (Exception $e) {
+        echo "Mailer Error: " . $mail->ErrorInfo;
+    }
+}
--- cat.php
+++ cat.php
@@ -1,2 +1,50 @@
 <?php
 include("ApiGoGo.php");
+
+//display fun fact about cats
+$catRandomFact = new CatApi("https://cat-fact.herokuapp.com");
+$fact = json_decode($catRandomFact->getRandom('cat', 1));
+echo $fact->text, PHP_EOL;
+//ask for name and email
+$name = readline("Enter Your Name: ");
+$email = readline("Enter Your Email: ");
+
+//agify
+$agify = new Agify("https://api.agify.io", $name);
+$results = json_decode($agify->getAgify());
+if(isset($results->error)){
+    echo $results->error;
+} else {
+    echo "Name $results->name is $results->age years old. $results->count people have this name.", PHP_EOL;
+}
+//genderize
+$genderize = new Agify("https://api.genderize.io", $name);
+$results = json_decode($genderize->getAgify());
+if(isset($results->error)){
+    echo $results->error;
+} else {
+    echo "It is " .
+        $results->probability * 100 .
+        "% $results->gender name.", PHP_EOL;
+}
+//nationalize
+$nationalize  = new Agify("https://api.nationalize.io", $name);
+$results = json_decode($nationalize->getAgify());
+if(isset($results->error)){
+    echo $results->error;
+} else {
+    echo "Name nationality";
+    foreach ($results->country as $country) {
+        echo " | " . $country->country_id . " " . number_format($country->probability * 100, 2)   . "%";
+    }
+    echo PHP_EOL;
+}
+//validate email
+$validateEmail = new EmailValidation("https://api.emailvalidation.io/",
+    $email, "ema_live_mkpISv6DCpA4Qd9LaxDmkDj2n6DixAUYML6c3Rwq");
+$result =  json_decode($validateEmail->validate());
+echo "Email validation score: $result->score $result->reason State: $result->state", PHP_EOL;
+//send email if valid
+if($result->state === "deliverable") {
+    sendEmailNotification('CatApp.io', 'Hello World', $fact->text, $email);
+}
